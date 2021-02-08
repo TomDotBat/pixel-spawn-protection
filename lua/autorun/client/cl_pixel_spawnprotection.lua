175b47f878e6074abf8be0c13e53bbc5c1f4bf23
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-08 13:32:16 +0000

    Stupidly over-optimised code, closes #1

--- lua/autorun/client/cl_pixel_spawnprotection.lua
+++ lua/autorun/client/cl_pixel_spawnprotection.lua
@@ -1,41 +1,64 @@
 
-local function loadAddon()
+local corner1, corner2 = Vector(3665, 1368, -196), Vector(2920, 498, 168) --Spawn area config
+
+--Set the isInSpawn metamethods
+local plyMeta = FindMetaTable("Player")
+local entMeta = FindMetaTable("Entity")
+
+local inSpawn = false
+local function isSelfInSpawn(s, verify)
+    if verify then
+        inSpawn = s:GetPos():WithinAABox(corner1, corner2)
+    end
+
+    return inSpawn
+end
+
+plyMeta.IsInSpawn = isSelfInSpawn
+entMeta.IsInSpawn = isSelfInSpawn
+
+--Drawing the spawn zone indicator
+local function loadUi()
     PIXEL.RegisterFont("SpawnProtection.Title", "Open Sans Bold", 42)
     PIXEL.RegisterFont("SpawnProtection.Description", "Open Sans Bold", 24)
 
-    local localPly
-    local inSpawn = false
-    local corner1, corner2 = Vector(3665, 1368, -196), Vector(2920, 498, 168)
-    timer.Create("PIXEL.SpawnProtection.AreaCheck", .5, 0, function()
-        if not IsValid(localPly) then
-            localPly = LocalPlayer()
-            return
-        end
-
-        inSpawn = localPly:GetPos():WithinAABox(corner1, corner2)
+    local centerX = ScrW() * .5
+    local titleX = PIXEL.Scale(80)
+    
+    local titleCol = PIXEL.Colors.Primary
+    local descriptionCol = PIXEL.Colors.PrimaryText
+    local textAlignCenter = TEXT_ALIGN_CENTER
+    
+    hook.Add("HUDPaint", "PIXEL.SpawnProtection.Indicator", function()
+        local _, titleH = PIXEL.DrawSimpleText("SPAWN", "PIXEL.SpawnProtection.Title", centerX, titleX, titleCol, textAlignCenter)
+        PIXEL.DrawSimpleText("You are in spawn, roleplay is not permitted here.", "PIXEL.SpawnProtection.Description", centerX, titleX + titleH, descriptionCol, textAlignCenter)
     end)
+end
 
-    local meta = FindMetaTable("Player")
-
-    function meta:IsInSpawn()
-        return inSpawn
-    end
-
-    hook.Add("HUDPaint", "PIXEL.SpawnProtection.Indicator", function(depth, skybox)
-        if skybox then return end
-        if not inSpawn then return end
-
-        local centerX = ScrW() * .5
-        local titleX = PIXEL.Scale(80)
-
-        local _, titleH = PIXEL.DrawSimpleText("SPAWN", "PIXEL.SpawnProtection.Title", centerX, titleX, PIXEL.Colors.Primary, TEXT_ALIGN_CENTER)
-        PIXEL.DrawSimpleText("You are in spawn, roleplay is not permitted here.", "PIXEL.SpawnProtection.Description", centerX, titleX + titleH, PIXEL.Colors.PrimaryText, TEXT_ALIGN_CENTER)
-    end)
+local function unloadUi()
+    hook.Remove("HUDPaint", "PIXEL.SpawnProtection.Indicator")
 end
 
 if PIXEL.UI then
-    loadAddon()
-    return
+    loadUi()
+else
+    hook.Add("PIXEL.UI.FullyLoaded", "PIXEL.SpawnProtection.WaitForPIXELUI", loadUi)
 end
 
-hook.Add("PIXEL.UI.FullyLoaded", "PIXEL.SpawnProtection.WaitForPIXELUI", loadAddon)
+--Perform the spawn area check
+local localPly
+timer.Create("PIXEL.SpawnProtection.AreaCheck", .5, 0, function()
+    if not IsValid(localPly) then
+        localPly = LocalPlayer()
+        return
+    end
+
+    local oldState = inSpawn
+    if isSelfInSpawn(localPly, true) == oldState then return end --If the state changes run the code below
+
+    net.Start("PIXEL.SpawnProtection.UpdateSpawnState")
+    net.SendToServer()
+        
+    if inSpawn then loadUi()
+    else unloadUi() end
+end)
