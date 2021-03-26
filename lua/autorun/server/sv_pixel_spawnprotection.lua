3e2274f72f4f75f984e6222fcc3517b9807ee3de
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-26 14:24:21 +0000

    Optimise serverside checks

--- lua/autorun/server/sv_pixel_spawnprotection.lua
+++ lua/autorun/server/sv_pixel_spawnprotection.lua
@@ -1,42 +1,52 @@
 
-local corner1, corner2 = Vector(-10284, 10948, -3004), Vector(-11506, 9924, -2440) --Spawn area config
-local corner3, corner4 = Vector(-9927, 10092, -30044), Vector(-10370, 10780, -2820)
-local verificationFrequency = 2
+local isSelfInSpawn
+local isValid = IsValid
 
---Set the isInSpawn metamethods
-local plyMeta = FindMetaTable("Player")
-local entMeta = FindMetaTable("Entity")
+do
+    local corner1, corner2 = Vector(-10284, 10948, -3004), Vector(-11506, 9924, -2440) --Spawn area config
+    local corner3, corner4 = Vector(-9927, 10092, -30044), Vector(-10370, 10780, -2820)
+    local verificationFrequency = 2
 
-local function isSelfInSpawn(s, verify)
-    if verify then
-        local inSpawn = s:GetPos():WithinAABox(corner1, corner2) or s:GetPos():WithinAABox(corner3, corner4)
-        s.PIXELIsInSpawn = inSpawn
+    function isSelfInSpawn(s, verify)
+        if verify then
+            local inSpawn = s:GetPos():WithinAABox(corner1, corner2) or s:GetPos():WithinAABox(corner3, corner4)
+            s.PIXELIsInSpawn = inSpawn
 
-        if not inSpawn then return inSpawn end --If they're in spawn we should create the serverside verification timer
+            if not inSpawn then return inSpawn end --If they're in spawn we should create the serverside verification timer
 
-        local ident = "PIXEL.SpawnProtection.AreaCheck:" .. s:SteamID64()
-        timer.Create(ident, verificationFrequency, 0, function() --Serverside verify every x seconds
-            if not IsValid(s) then
-                timer.Remove(ident)
-                return
+            local veh = s:GetVehicle()
+            if isValid(veh) then
+                SafeRemoveEntityDelayed(veh, 0)
             end
 
-            if not isSelfInSpawn(s, true) then --They're no longer in spawn so we've changed the state manually and removed the timer
-                timer.Remove(ident)
-            end
-        end)
+            local ident = "PIXEL.SpawnProtection.AreaCheck:" .. s:SteamID64()
+            timer.Create(ident, verificationFrequency, 0, function() --Serverside verify every x seconds
+                if not IsValid(s) then
+                    timer.Remove(ident)
+                    return
+                end
+
+                if not isSelfInSpawn(s, true) then --They're no longer in spawn so we've changed the state manually and removed the timer
+                    timer.Remove(ident)
+                end
+            end)
+        end
+
+        return s.PIXELIsInSpawn
     end
-
-    return s.PIXELIsInSpawn
 end
 
-plyMeta.IsInSpawn = isSelfInSpawn
-entMeta.IsInSpawn = isSelfInSpawn
+do --Set the isInSpawn metamethods
+    local plyMeta = FindMetaTable("Player")
+    local entMeta = FindMetaTable("Entity")
+    plyMeta.IsInSpawn = isSelfInSpawn
+    entMeta.IsInSpawn = isSelfInSpawn
+end
 
 --Spawn state checking
 hook.Add("PlayerSpawn", "PIXEL.SpawnProtection.ResetState", function(ply, attacker)
     timer.Simple(0, function() --Wait 1 tick to check their pos and update their inSpawn state
-        if not IsValid(ply) then return end
+        if not isValid(ply) then return end
         isSelfInSpawn(ply, true)
     end)
 end)
