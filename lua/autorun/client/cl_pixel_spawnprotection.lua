30b1187e513248260d442449f23a532a0aa67435
Tom O'Sullivan <tom@tomdotbat.dev>
2021-10-27 20:18:47 +0100

    chore: add the license

--- lua/autorun/client/cl_pixel_spawnprotection.lua
+++ lua/autorun/client/cl_pixel_spawnprotection.lua
@@ -1,4 +1,19 @@
 
+--[[
+    PIXEL Spawn Protection
+
+    Copyright (C) 2021 Tom O'Sullivan (Tom.bat)
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]
+
 local corner1, corner2 = Vector(-10284, 10948, -3004), Vector(-11506, 9924, -2440) --Spawn area config
 local corner3, corner4 = Vector(-9927, 10092, -30044), Vector(-10370, 10780, -2820)
 
